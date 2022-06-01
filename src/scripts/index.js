d6a1e34acfebfc54cfa8260c6c14bdb389e02a96
RizqiMKS <rizqimks@gmail.com>
2022-06-01 22:22:12 +0700

    Adding drawer-initiator

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,3 +3,18 @@ import '../styles/main.css';
 import '../styles/responsive.css';
 import './views/components/appbar-element';
 import './views/components/hero-element';
+import App from './views/app';
+
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+});
